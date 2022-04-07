6ac64491d70b76a7e18b1067abca0f564007eeaf
Nicolai Haug <nicolaihaug@yahoo.com>
2022-04-07 16:19:15 +0200

    Example usage

---
+++ main_metropolis.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from src import vmc
+
+
+def exact_energy(N, dim, omega):
+    return (omega * dim * N) / 2
+
+
+def safe_initial_positions(wavefunction, alpha, N, dim, seed=None):
+    rng = np.random.default_rng(seed=seed)
+    positions = rng.random(size=(N, dim))
+    # safe initialization
+    wf2 = wavefunction.pdf(positions, alpha)
+    while np.sum(wf2) <= 1e-14:
+        positions *= 0.5
+        wf2 = wavefunction.pdf(positions, alpha)
+    return positions
+
+
+N = 500        # Number of particles
+dim = 3      # Dimensionality
+omega = 1.   # Oscillator frequency
+
+# Instantiate wave function
+# Analytical
+wf = vmc.NIBWF(N, dim, omega)
+# Numerical
+#wf = vmc.LogNIB(omega)
+
+# Instantiate sampler
+sampler = vmc.Metropolis(wf)
+
+# Config
+nsamples = 40000
+initial_alpha = 0.2
+
+# Set intial positions
+initial_positions = safe_initial_positions(wf, initial_alpha, N, dim)
+
+start = time.time()
+results = sampler.sample(nsamples,
+                         initial_positions,
+                         initial_alpha,
+                         scale=1.0,
+                         nchains=4,
+                         warm=True,
+                         warmup_iter=500,
+                         tune=True,
+                         tune_iter=5000,
+                         tune_interval=250,
+                         tol_tune=1e-5,
+                         optimize=True,
+                         max_iter=70000,
+                         batch_size=500,
+                         gradient_method='adam',
+                         eta=0.01,
+                         tol_optim=1e-5,
+                         )
+
+end = time.time()
+print("Sampler elapsed time:", end - start)
+
+exact_E = exact_energy(N, dim, omega)
+print(f"Exact energy: {exact_E}")
+print(results)
